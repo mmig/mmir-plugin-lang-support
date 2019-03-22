bac005a0473335beed33629ec6c3d0185bc8b89c
russa <aaron.russ@dfki.de>
2019-03-22 20:32:42 +0100

    add callback to async function installFiles

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ tools for querying supported languages (ASR and TTS) and voices (TTS)
 
 NOTE: for usage in cordova-plugins, copy /www or /src to appropriate
       location in plugin, e.g. via the exported
-      function `installFiles(srcDirType: "src" | "www", targetDir: string)`
+      function `installFiles(srcDirType: "src" | "www", targetDir: string, callback(err: Error | null))`
--- install.js
+++ install.js
@@ -5,15 +5,26 @@ var path = require('path');
 
 var srcDirRoot = __dirname;
 
-function copyFiles(srcDirType, targetDir){
+function copyFiles(srcDirType, targetDir, callback){
 
   var srcDir = path.join(srcDirRoot, srcDirType);
-  fs.readDir(srcDir, function(files){
+  fs.readdir(srcDir, function(err, files){
+    if(err){
+      if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying files, could not read source directory '+srcDir+': ', err);
+      return callback && callback(err);
+    }
+    var count = files.length;
+    if(count === 0) callback && callback(null);
     files.forEach(function(f){
-      console.log('mmir-plugin-lang-support.install(): copying '+f+' -> ', targetDir);
+      if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): copying '+f+' -> ', targetDir);
       fs.copyFile(path.join(srcDir, f), path.join(targetDir, f), function(err){
+        --count;
         if(err){
-          console.log('mmir-plugin-lang-support.install(): ERROR copying '+f+': ', err);
+          if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying '+f+': ', err);
+          return callback && callback(err);
+        }
+        if(count === 0){
+          callback && callback(null);
         }
       })
     });
