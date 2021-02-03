72c9e68489885e4d465b082174c08ce007628776
russa <aaron.russ@dfki.de>
2021-02-03 22:55:11 +0100

    support absolute source path for copying files

--- install.js
+++ install.js
@@ -7,20 +7,21 @@ var srcDirRoot = __dirname;
 
 function copyFiles(srcDirType, targetDir, callback){
 
-  var srcDir = path.join(srcDirRoot, srcDirType);
+  var srcDir = path.isAbsolute(srcDirType)? srcDirType : path.join(srcDirRoot, srcDirType);
+  var verbose = process.env.verbose;
   fs.readdir(srcDir, function(err, files){
     if(err){
-      if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying files, could not read source directory '+srcDir+': ', err);
+      if(verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying files, could not read source directory '+srcDir+': ', err);
       return callback && callback(err);
     }
     var count = files.length;
     if(count === 0) callback && callback(null);
     files.forEach(function(f){
-      if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): copying '+f+' -> ', targetDir);
+      if(verbose) console.log('mmir-plugin-lang-support.install(): copying '+f+' -> ', targetDir);
       fs.copyFile(path.join(srcDir, f), path.join(targetDir, f), function(err){
         --count;
         if(err){
-          if(process.env.verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying '+f+': ', err);
+          if(verbose) console.log('mmir-plugin-lang-support.install(): ERROR copying '+f+': ', err);
           return callback && callback(err);
         }
         if(count === 0){
